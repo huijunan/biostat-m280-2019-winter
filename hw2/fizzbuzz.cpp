f822cdbf1ca0536ac385f32a377a78be0357798e
huijun.an <huijun.an@ucla.edu>
2019-02-08 23:29:47 +0000

    final polishment

--- hw2/fizzbuzz.cpp
+++ hw2/fizzbuzz.cpp
@@ -9,19 +9,19 @@ int main()
 
 // [[Rcpp::export]]
 void RCPP_fizzbuzz(vector<double> & input){
-  for(unsigned i = 0; i < input.size(); i++){
-    if(input[i] != floor(input[i])){
+  for (unsigned i = 0; i < input.size(); i++){
+    if (input[i] != floor(input[i])){
       Rcout << "not valid input, please try again" << endl;
       return;
     }
     int iter = int(input[i]);
-    if(iter % 15 == 0){
+    if (iter % 15 == 0){
       Rcout << "FizzBuzz" << endl;
     }
-    else if(iter  % 3 == 0){
+    else if (iter  % 3 == 0){
       Rcout << "Fizz" << endl;
     }
-    else if(iter  % 5 == 0){
+    else if (iter  % 5 == 0){
       Rcout << "Buzz" << endl;
     }
     else{
